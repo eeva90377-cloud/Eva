300d15bd8e3db41138e65b931eaab663e39f8e57
eeva90377-cloud <eeva90377@gmail.com>
2026-02-11 14:57:01 +0100

    Add files via upload

---
+++ exampl.py
@@ -0,0 +1,21 @@
+a=21
+b=10
+c=0
+c=a+b
+print ("a: {} b: {} a+b: {} ".formart(a,b,c))
+c=a-b
+print ("a: {} b: {} a-b: {} ".formart(a,b,c))
+c=a*b
+print ("a: {} b: {} a*b: {} ".formart(a,b,c))
+c=a/b
+print ("a: {} b: {} a/b: {} ".formart(a,b,c))
+c=a%b
+print ("a: {} b: {} a%b: {} ".formart(a,b,c))
+a=2
+b=3
+c=a**b
+print ("a: {} b: {} a**b: {} ".formart(a,b,c))
+a=10
+b=5
+c=a//b
+print ("a: {} b: {} a/b: {} ".formart(a,b,c))
